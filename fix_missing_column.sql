9a0285b41b73b90faea9b9fa4c6f591e05df0fd5
kutteddula kasi viswanath reddy <68494319+khasi55@users.noreply.github.com>
2026-01-03 20:13:26 +0400

    Fix typo in column name for transactions table

--- fix_missing_column.sql
+++ fix_missing_column.sql
@@ -1,2 +1,2 @@
 -- Run this in your Supabase SQL Editor to fix the "column does not exist" error
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS merchant_upi_id text;
+ALTER TABLE transactions ADD COLUMN IF NOT EXISTS merchan t_upi_id text;
