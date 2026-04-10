0f1e93372ffceaa833d9e3ab0bcb2a59f4f19c1d
kutteddula kasi viswanath reddy <68494319+khasi55@users.noreply.github.com>
2026-04-11 01:23:31 +0530

    Update upiConfig.ts

--- src/config/upiConfig.ts
+++ src/config/upiConfig.ts
@@ -4,7 +4,7 @@ export interface UpiConfig {
 }
 
 export const UPI_CONFIGS: UpiConfig[] = [
-    { vpa: 'jaspprosolutions@idbi', merchantName: 'Jaspro Solutions' },
+    { vpa: 'adrshine05553@iob', merchantName: 'adrshine' },
 ];
 
 /**
