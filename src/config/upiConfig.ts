d622bc4ef2a37f03df0fced09d536cc75396c665
kutteddula kasi viswanath reddy <68494319+khasi55@users.noreply.github.com>
2026-04-22 00:34:05 +0530

    Update upiConfig.ts

--- src/config/upiConfig.ts
+++ src/config/upiConfig.ts
@@ -4,7 +4,7 @@ export interface UpiConfig {
 }
 
 export const UPI_CONFIGS: UpiConfig[] = [
-    { vpa: 'ADRSHINE.10019962@csbpay', merchantName: 'ADRSHINE' },
+    { vpa: 'groundzero24@ibl', merchantName: 'Sharkfunded' },
 ];
 
 /**
